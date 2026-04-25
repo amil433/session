a57393da262d727b5d78f96133674ea3a3bc8bfa
amil433 <151804426+amil433@users.noreply.github.com>
2026-04-25 17:49:37 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+const { default: makeWASocket, useMultiFileAuthState } = require("@whiskeysockets/baileys")
+
+async function start() {
+  const { state, saveCreds } = await useMultiFileAuthState("./session")
+
+  const sock = makeWASocket({
+    auth: state,
+    printQRInTerminal: true
+  })
+
+  // simpan session otomatis
+  sock.ev.on("creds.update", saveCreds)
+
+  // status koneksi
+  sock.ev.on("connection.update", (update) => {
+    const { connection } = update
+    console.log("STATUS:", connection)
+
+    if (connection === "open") {
+      console.log("✅ SESSION BERHASIL DIBUAT")
+    }
+
+    if (connection === "close") {
+      console.log("❌ KONEKSI PUTUS")
+    }
+  })
+}
+
+start()
